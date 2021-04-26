edf573c3eafcd8e946b1f88a2b06f8cc16053791
Christian Fenzl <fenzl@t-r-t.at>
2021-04-26 21:46:01 +0200

    Shelly Transformer: Tunable White
    https://www.loxwiki.eu/x/_QBABQ

--- data/transform/shipped/udpin/shelly/shelly_rgb&w.php
+++ data/transform/shipped/udpin/shelly/shelly_rgb&w.php
@@ -21,25 +21,39 @@
 	
 	$data = array (	
 		'effect' => 0,
-		'gain' => 100,
 		'turn' => 'on',
-		'mode' => 'color'
 	);
 	
 	switch ($command) {
 		// Color mode
 		case 'white': 
 			$white = round( $value_pct / 100 * 255 );
+			$data['mode'] = "color";
+			$data['gain'] = 100;
 			$data['white'] = $white;
 			break;
+
 		case 'rgb':
 			$rgb_pct = str_pad( $value_pct, 9, '0', STR_PAD_LEFT );
 			$red = round( substr( $rgb_pct, -3, 3) / 100 * 255 );
 			$green = round( substr( $rgb_pct, -6, 3) / 100 * 255 );
 			$blue = round( substr( $rgb_pct, -9, 3) / 100 * 255 );
+			$data['mode'] = "color";
+			$data['gain'] = 100;
 			$data['red'] = $red;
 			$data['green'] = $green;
 			$data['blue'] = $blue;
+			break;
+
+		case 'tunablew':
+			$tunable = str_pad( $value_pct, 9, '0', STR_PAD_LEFT );
+			$bright = substr( $tunable, -7, 3);
+			$temp = substr( $tunable, -4, 4);
+			// Normalize Lumitech 2700...6500 to Shelly 3000...6500
+			$temp = round( 3000 + ( $temp-2700 ) * (6500-3000) / (6500-2700) );
+			$data['mode'] = "white";
+			$data['temp'] = $temp;
+			$data['brightness'] = intval($bright);
 			break;
 		default:
 			error_log('Transformer shelly_rgb&w: Wrong parameters (white or rgb missing)');
